2e117488343c7a494a6844d76c977d37882ebe62
Dionisio Chiuratto Agourakis <dionisio@jquant.com.br>
2025-02-25 19:21:47 -0300

    Bump version to 0.3.0 for release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_namespace_packages
 
 if __name__ == "__main__":
     setup(
-        version="0.3.0.dev1",
+        version="0.3.0",
         packages=find_namespace_packages(include=["ragatanga", "ragatanga.*"]),
         # The rest of the configuration is in pyproject.toml
     ) 
